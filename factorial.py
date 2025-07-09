1ae7015e93c86f6b7e54bc1cc28bf66b3a6f3931
IMugisha-io <isaacmugisha2019@gmail.com>
2025-07-09 17:10:56 +0300

    4! passed with the new code

--- factorial.py
+++ factorial.py
@@ -3,13 +3,28 @@
 # For example, the factorial of 5 (written as 5!) is 5 * 4 * 3 * 2 * 1 = 120. 
 # The factorial of 0 (0!) is defined as 1
 
-#-------------0!--------------------------
+#-------------0! & 1!--------------------------
+
+#def factorial (a):
+#    return 1
+
+
+'''-------------2! & 3!--------------------------
 def factorial (a):
     if a < 2 :
         result = 1
     else :
         result = a * (a-1)
-    return result
+    return result '''
         
-    
+#-------------4!--------------------------  
+def factorial (a):
+    if a < 2 :
+        result = 1
+    else :
+        product = 1 
+        for i in range(1,a+1):
+            product*=i
+            result = product
         
+    return result 
--- test_factorial.py
+++ test_factorial.py
@@ -8,3 +8,9 @@ def test_factorial_1 ():
 
 def test_factorial_2 ():
     assert factorial (2) == 2
+
+def test_factorial_3 ():
+    assert factorial (3) == 6
+
+def test_factorial_4 ():
+    assert factorial (4) == 24
