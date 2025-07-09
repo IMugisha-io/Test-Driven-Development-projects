0b6efca04e60355ae893e81a29ca2e851f5e4e62
IMugisha-io <isaacmugisha2019@gmail.com>
2025-07-09 12:30:03 +0300

    stretch exercise

--- app/multiply.py
+++ app/multiply.py
@@ -1,2 +1,12 @@
-def multiply (a,b):
-    return a*b
+#def multiply (a,b):
+#    return a*b
+
+# Stretch Exercise: What might you do if your language didn't natively support multiply?
+
+def multiply(a, b):
+    total = 0
+    for _ in range(b):
+        total += a
+    return total
+
+   
