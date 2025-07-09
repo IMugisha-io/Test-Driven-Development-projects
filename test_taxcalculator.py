f2fed2b7a439a36faec3fb3cbfe68c1740610d91
IMugisha-io <isaacmugisha2019@gmail.com>
2025-07-09 22:39:17 +0300

    test at 0 passed :green

---
+++ test_taxcalculator.py
@@ -0,0 +1,10 @@
+#If earnings < 12000 per year, pay no tax
+#then with earnings between 12,000 and 36,000 pay 20% tax
+#then with earnings greater than 36000 per year, pay 40% tax
+#Use a test first approach.  Commit at least after every passing test.
+
+from taxCalculator import taxcalculator
+
+def test_taxcalculator_0():
+    assert taxcalculator(0)==0
+
